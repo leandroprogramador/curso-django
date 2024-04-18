4b92ccb1add1003afed68961a75f4a4774f016e3
Leandro Araujo <lean.ib@hotmail.com>
2024-04-18 14:23:06 -0300

    Adicionado font-awesome e criacao de partial head

--- recipes/views.py
+++ recipes/views.py
@@ -4,13 +4,7 @@ from django.shortcuts import render
 # Create your views here.
 
 def home(request):
-    return render(request, 'recipes/home.html', context= {
+    return render(request, 'recipes/pages/home.html', context= {
         'name' : 'Leandro Araujo'
     })
-
-def contato(request):
-    return HttpResponse('Contato')
-
-def sobre(request):
-    return HttpResponse('SOBRE')
 
