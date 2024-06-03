df6b37011778be41558b3b2cb6977936be80bce3
Leandro Araujo <lean.ib@hotmail.com>
2024-06-03 01:10:49 -0300

    Separating templates with heritance and inject mock content in recipes views

--- recipes/views.py
+++ recipes/views.py
@@ -1,10 +1,18 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from utils.recipes.factory import make_recipe
+
 # Create your views here.
 
 def home(request):
     return render(request, 'recipes/pages/home.html', context= {
-        'name' : 'Leandro Araujo'
+        'recipes' : [make_recipe() for _ in range(13)]
+    })
+
+def recipes(request, id):
+    return render(request, 'recipes/pages/recipe-view.html', context= {
+        'recipe' : make_recipe(),
+        'is_detail_page' : True
     })
 
